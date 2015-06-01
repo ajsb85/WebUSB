b7e0569dfbb18aa559d3355adf20c4a32636a9a4
autonome <autonome@gmail.com>
2015-06-01 15:02:17 -0700

    Starting to experiment with Mac support

--- chrome/content/main.js
+++ chrome/content/main.js
@@ -12,9 +12,9 @@ var file = Components.classes["@mozilla.org/file/directory_service;1"].
            get("CurProcD", Components.interfaces.nsIFile);
 
 // Append the file name.
-file.append("hidapi.dll");
+file.append("hidapi-mac");
 
-dump(file.path);
+//dump(file.path + '\n');
 
 var lib = ctypes.open(file.path);
 
@@ -24,40 +24,59 @@ var lib = ctypes.open(file.path);
 	devs = hid_enumerate(0x0, 0x0); */
   
 var hid =  lib.declare("hid_init",
-                        ctypes.winapi_abi,
-                        ctypes.bool      /* return type */);
+                        ctypes.default_abi,
+                        ctypes.bool); // return type
 var struct_hid_device_info;
 
   struct_hid_device_info = ctypes.StructType('hid_device_info', [
     {'path': ctypes.char.ptr},
     {'vendor_id': ctypes.unsigned_short},
     {'product_id': ctypes.unsigned_short},
-    {'serial_number': ctypes.jschar.ptr},
+    {'serial_number': ctypes.unsigned.ptr},
     {'release_number': ctypes.unsigned_short},
-    {'manufacturer_string': ctypes.jschar.ptr},
-    {'product_string': ctypes.jschar.ptr},
+    {'manufacturer_string': ctypes.unsigned.ptr},
+    {'product_string': ctypes.unsigned.ptr},
     {'usage_page': ctypes.unsigned_short},
     {'usage': ctypes.unsigned_short},
     {'interface_number': ctypes.int}
   ]);  
 /* Declare the signature of the function we are going to call */
 var enumerate = lib.declare("hid_enumerate",
-                         ctypes.winapi_abi,
+                         ctypes.default_abi,
                          struct_hid_device_info,
                          ctypes.int32_t,
                          ctypes.int32_t);
 
 hid();
+/*
 var log_enumerate = enumerate(0, 0); 
 
-dump(struct_hid_device_info);
+dump(struct_hid_device_info + '\n');
 // 20 on 32-bit, 32 on 64-bit if I'm not mistaken
 
-//dump(log_enumerate);
+dump(log_enumerate + '\n');
+*/
+
+var devs = enumerate(0, 0),
+    cur_dev = devs
+while (cur_dev) {
+  dump("Device Found\n  type: %04hx %04hx\n  path: %s\n  serial_number: %ls", cur_dev.vendor_id, cur_dev.product_id, cur_dev.path, cur_dev.serial_number);
+  dump("\n");
+  dump("  Manufacturer: %ls\n", cur_dev.manufacturer_string);
+  dump("  Product:      %ls\n", cur_dev.product_string);
+  dump("  Release:      %hx\n", cur_dev.release_number);
+  dump("  Interface:    %d\n",  cur_dev.interface_number);
+  dump("\n");
+  if (cur_dev.next)
+    cur_dev = devs.next()
+  else
+    cur_dev = null
+}
 
 lib.close();
 
 /* Some constants used with the Windows API. */
+/*
 const HWND_TOPMOST = -1;
 const HWND_NOTOPMOST = -2;
 
@@ -68,20 +87,21 @@ var aotUser32Lib = ctypes.open("user32");
 
 var setWindowPos = aotUser32Lib.declare("SetWindowPos",
                                   ctypes.winapi_abi,
-                                  ctypes.bool,      /* return type */
-                                  ctypes.uint32_t,  /* HWND hWnd */
-                                  ctypes.int32_t,   /* HWND hWndInsertAfter */
-                                  ctypes.int32_t,   /* int x */
-                                  ctypes.int32_t,   /* int y */
-                                  ctypes.int32_t,   /* int cx */
-                                  ctypes.int32_t,   /* int cy */
-                                  ctypes.uint32_t   /* UINT uFlags */
+                                  ctypes.bool,      // return type
+                                  ctypes.uint32_t,  // HWND hWnd
+                                  ctypes.int32_t,   // HWND hWndInsertAfter
+                                  ctypes.int32_t,   // int x
+                                  ctypes.int32_t,   // int y
+                                  ctypes.int32_t,   // int cx
+                                  ctypes.int32_t,   // int cy
+                                  ctypes.uint32_t   // UINT uFlags
                                  );
 
 var getActiveWindow = aotUser32Lib.declare("GetActiveWindow",
                                      ctypes.winapi_abi,
-                                     ctypes.uint32_t /* return type HWND */
+                                     ctypes.uint32_t // return type HWND
                                     );
+*/
 
 var aot = {
   prefs: {},
@@ -111,7 +131,7 @@ var aot = {
 
   load: function() {
     // Get and apply saved status
-    aot.setWindowToTop(true);
+    //aot.setWindowToTop(true);
 
     // We want to set the always on top flag after minimize/restore again
     window.addEventListener("activate", aot.setFromMenu, false);
